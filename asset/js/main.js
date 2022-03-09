7c7b5ba7e7261a3f352bda9d2550081ed2a7c0b3
AlbertoBiga <alberto.biga10@gmail.com>
2022-03-09 16:39:20 +0100

    richiesta info preliminari e calcolo prezzo senza sconti età

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,4 +1,14 @@
-// 1) KM DA PERCORRERE
+// 1) INFO PRELIMINARI (KM ED ETA')
+
+const km = prompt ("Quanti kilometri desideri effettuare? INSERIRE KM IN NUMERO")
+console.log(km)
+
+const anni = prompt ("Quanti anni hai? INSERIRE ETA' IN NUMERO")
+console.log(anni)
+
 // 2) PREZZO = KM * 0.21
+
+const prezzo = ( km * 0.21 )
+console.log(prezzo)
 // 3) SCONTO 20% MINORENNE SCONTO 40% OVER 65
 // 4) OUTPUT PREZZO FINALE 2 CIFRE DECIMALI
