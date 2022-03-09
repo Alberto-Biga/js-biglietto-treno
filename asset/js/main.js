adc2d25fca1a18ba62cb93717276b0eb5c4fcfb6
AlbertoBiga <alberto.biga10@gmail.com>
2022-03-09 17:28:02 +0100

    output prezzo finale arrotondato

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,5 +1,4 @@
 // 1) INFO PRELIMINARI (KM ED ETA')
-
 const km = prompt ("Quanti kilometri desideri effettuare? INSERIRE KM IN NUMERO")
 console.log(km)
 
@@ -8,29 +7,34 @@ console.log(anni)
 
 
 // 2) PREZZO = KM * 0.21
-
 const prezzo = ( km * 0.21 )
 console.log(prezzo)
 
 
 // 3) SCONTO 20% MINORENNE SCONTO 40% OVER 65
 const prezzoMinorenne = ( prezzo - ( prezzo * 0.2 ))
-console.log( 'prezzo minorenne  ' + prezzoMinorenne)
+const prezzoMinorenneArrotondato = prezzoMinorenne.toFixed(2) //PREZZI ARROTONDATI 2 CIFRE DECIMALI
+console.log( 'prezzo minorenne  ' + prezzoMinorenneArrotondato)
 
 const prezzoOver65 = ( prezzo - ( prezzo * 0.4 ))
-console.log( 'prezzo over 65  ' + prezzoOver65)
+const prezzoOver65Arrotondato = prezzoOver65.toFixed(2) //PREZZI ARROTONDATI 2 CIFRE DECIMALI
+console.log( 'prezzo over65  ' + prezzoOver65Arrotondato)
 
 const prezzoStandard = prezzo
+const prezzoStandardArrotondato = prezzoStandard.toFixed(2) //PREZZI ARROTONDATI 2 CIFRE DECIMALI
 console.log('prezzo standard  ' + prezzoStandard)
 
+
+//OUTPUT FINALE
 if ( anni < 18 ) {
+    document.getElementById("target-container").innerHTML = "Il prezzo è di " + prezzoMinorenneArrotondato + " euro";
     console.log ('prezzo definitivo  ' + prezzoMinorenne)
 } else if ( anni > 65 ) {
+    document.getElementById("target-container").innerHTML = "Il prezzo è di " + prezzoOver65Arrotondato + " euro";
     console.log ('prezzo definitivo  ' + prezzoOver65)
 } else {
+    document.getElementById("target-container").innerHTML = "Il prezzo è di " + prezzoStandardArrotondato + " euro";
     console.log ('prezzo definitivo  ' + prezzoStandard)
 }
 
 
-
-// 4) OUTPUT PREZZO FINALE 2 CIFRE DECIMALI
