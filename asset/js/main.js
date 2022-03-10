65e2dff640f65387ec4603c15f525f8aa47fae2d
AlbertoBiga <alberto.biga10@gmail.com>
2022-03-10 10:49:05 +0100

    correzione

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,40 +1,46 @@
 // 1) INFO PRELIMINARI (KM ED ETA')
-const km = prompt ("Quanti kilometri desideri effettuare? INSERIRE KM IN NUMERO")
+let km = prompt ("Quanti kilometri desideri effettuare? INSERIRE KM IN NUMERO")
 console.log(km)
+// MODIFICO VALORE DA STRINGA A NUMERO
+km = Number(km)
+console.log(typeof km)
 
-const anni = prompt ("Quanti anni hai? INSERIRE ETA' IN NUMERO")
+let anni = prompt ("Quanti anni hai? INSERIRE ETA' IN NUMERO")
 console.log(anni)
+// MODIFICO VALORE DA STRINGA A NUMERO
+anni = Number(anni)
+console.log(typeof anni)
 
 
 // 2) PREZZO = KM * 0.21
-const prezzo = ( km * 0.21 )
-console.log(prezzo)
+let prezzo = ( km * 0.21 )
+console.log( prezzo)
 
 
 // 3) SCONTO 20% MINORENNE SCONTO 40% OVER 65
-const prezzoMinorenne = ( prezzo - ( prezzo * 0.2 ))
-const prezzoMinorenneArrotondato = prezzoMinorenne.toFixed(2) //PREZZI ARROTONDATI 2 CIFRE DECIMALI
+let prezzoMinorenne = ( prezzo - ( prezzo * 0.2 ))
+let prezzoMinorenneArrotondato = prezzoMinorenne.toFixed(2) //PREZZI ARROTONDATI 2 CIFRE DECIMALI
 console.log( 'prezzo minorenne  ' + prezzoMinorenneArrotondato)
 
-const prezzoOver65 = ( prezzo - ( prezzo * 0.4 ))
-const prezzoOver65Arrotondato = prezzoOver65.toFixed(2) //PREZZI ARROTONDATI 2 CIFRE DECIMALI
+let prezzoOver65 = ( prezzo - ( prezzo * 0.4 ))
+let prezzoOver65Arrotondato = prezzoOver65.toFixed(2) //PREZZI ARROTONDATI 2 CIFRE DECIMALI
 console.log( 'prezzo over65  ' + prezzoOver65Arrotondato)
 
-const prezzoStandard = prezzo
-const prezzoStandardArrotondato = prezzoStandard.toFixed(2) //PREZZI ARROTONDATI 2 CIFRE DECIMALI
-console.log('prezzo standard  ' + prezzoStandard)
+let prezzoStandard = prezzo
+let prezzoStandardArrotondato = prezzoStandard.toFixed(2) //PREZZI ARROTONDATI 2 CIFRE DECIMALI
+console.log('prezzo standard  ' + prezzoStandardArrotondato)
 
 
 //OUTPUT FINALE
 if ( anni < 18 ) {
     document.getElementById("target-container").innerHTML = "Il prezzo è di " + prezzoMinorenneArrotondato + " euro";
-    console.log ('prezzo definitivo  ' + prezzoMinorenne)
+    console.log ('prezzo definitivo  ' + prezzoMinorenneArrotondato)
 } else if ( anni > 65 ) {
     document.getElementById("target-container").innerHTML = "Il prezzo è di " + prezzoOver65Arrotondato + " euro";
-    console.log ('prezzo definitivo  ' + prezzoOver65)
+    console.log ('prezzo definitivo  ' + prezzoOver65Arrotondato)
 } else {
     document.getElementById("target-container").innerHTML = "Il prezzo è di " + prezzoStandardArrotondato + " euro";
-    console.log ('prezzo definitivo  ' + prezzoStandard)
+    console.log ('prezzo definitivo  ' + prezzoStandardArrotondato)
 }
 
 
